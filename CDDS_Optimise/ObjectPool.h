1fcaa87c1af64bb5f26be5d8806c3c582de7c4f1
KevinHall2 <khjiitech@gmail.com>
2025-02-26 17:01:46 -0600

    defined the declarations of the Release(), Get(), Clear(), and the ObjectPool() constructor in the ObjectPool.h file. additionally declared unit tests in its own project for ObjectPool.h

--- CDDS_Optimise/ObjectPool.h
+++ CDDS_Optimise/ObjectPool.h
@@ -22,7 +22,7 @@ public:
 	//determines if something was released back to the pool or not
 	//opposite of Get(), moves from the active list to the inactive list
 	//returns the thing<T> that was just moved
-	//also checks if something is already in the inactive list, and if it is, doesn't add it in again
+	//also checks if the thing<T> is already in the inactive list, and if it is, doesn't add it in again
 	bool Release(T value);
 
 
@@ -41,12 +41,19 @@ inline ObjectPool<T>::ObjectPool(int size, CreateItemSignature createItemFunctio
 {
 	m_createItemFunction = createItemFunction;
 	//initialize active and inactive lists or check if they are initialized
+	if (!m_inactiveList)
+		private List<T> m_inactiveList;
+	if (!m_activeList)
+		private List<T> m_activeList;
 	//also check if int size is a valid number
+	if (size <= 0)
+		return null;
 
 	for (int i = 0; i < size; i++)
 	{
 		T item = createItemFunction();
 		//then add the newly made item to the inactive list
+		m_inactveList.pushFront(item);
 	}
 }
 
@@ -58,21 +65,41 @@ inline ObjectPool<T>::~ObjectPool()
 template<typename T>
 inline void ObjectPool<T>::Clear()
 {
+	m_inactiveList.~List();
+	m_activeList.~List();
 }
 
 template<typename T>
 inline T ObjectPool<T>::Get()
 {
 	//if the object pool is empty, make a new item to put into the inactive list
-	//T item = m_createItemFunction();
+	if (getInactiveCount() <= 0)
+	{
+	  T item = m_createItemFunction();
+	  m_inactiveList.pushFront(item);
+	}
 	//if the pool is not empty, get an item from the inactive list and put it into the active list
+	else
+	{
+		T item = m_inactiveList.begin();
+		m_activeList.pushFront(item);
+		m_inactiveList.popFront();
+	}
 	return item;
 }
 
 template<typename T>
 inline bool ObjectPool<T>::Release(T value)
 {
-	return false;
+	if (m_inactiveList.find(value))
+		break;
+	else
+	{
+		T item = m_activeList.find(value);
+		m_inactiveList.pushFront(item);
+		m_activeList.remove(item);
+	}
+	return item;
 }
 
 template<typename T>
---
+++ ObjectPoolTests/ObjectPoolTests.cpp
@@ -0,0 +1,35 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../CDDS_Optimise/ObjectPool.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ObjectPoolTests
+{
+	TEST_CLASS(ObjectPoolTests)
+	{
+	public:
+		
+		TEST_METHOD(ObjectPoolConstructor)
+		{
+
+		}
+
+		TEST_METHOD(Clear)
+		{
+
+		}
+
+		TEST_METHOD(Release)
+		{
+
+		}
+
+		TEST_METHOD(Get)
+		{
+
+		}
+
+		
+	};
+}
